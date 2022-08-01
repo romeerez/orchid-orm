c89cd8b6230affd5ded20fb05fadbddbf15e489e
romeerez <romadzao@gmail.com>
2022-08-01 05:09:46 -0400

    Refactor .with to separate class

--- packages/pqb/src/queryMethods.ts
+++ packages/pqb/src/queryMethods.ts
@@ -1,14 +1,6 @@
-import {
-  EMPTY_OBJECT,
-  Expression,
-  isRaw,
-  raw,
-  RawExpression,
-  StringKey,
-} from './common';
+import { Expression, raw, RawExpression, StringKey } from './common';
 import {
   AddQueryJoinedTable,
-  AddQueryWith,
   ColumnsParsers,
   Query,
   QueryWithData,
@@ -31,7 +23,6 @@ import {
   UnionArg,
   WhereItem,
   WindowArg,
-  WithOptions,
 } from './sql';
 import {
   pushQueryArray,
@@ -48,57 +39,16 @@ import {
   thenValue,
   thenVoid,
 } from './thenMethods';
-import { Db } from './db';
 import {
   ColumnShapeOutput,
-  ColumnsShape,
   ColumnType,
-  ColumnTypes,
   NumberColumn,
   StringColumn,
 } from './columnSchema';
 import { SelectMethods } from './selectMethods';
 import { FromMethod } from './fromMethod';
+import { WithMethod } from './withMethod';
 import { JsonMethods } from './jsonMethods';
-
-type WithArgsOptions = Omit<WithOptions, 'columns'> & {
-  columns?: boolean | string[];
-};
-
-type WithArgs =
-  | [string, ColumnsShape, RawExpression]
-  | [string, WithArgsOptions, ColumnsShape, RawExpression]
-  | [string, Query | ((qb: Db) => Query)]
-  | [string, WithArgsOptions, Query | ((qb: Db) => Query)];
-
-type WithShape<Args extends WithArgs> = Args[1] extends Query
-  ? Args[1]['result']
-  : Args[1] extends (qb: Db) => Query
-  ? ReturnType<Args[1]>['result']
-  : Args[2] extends Query
-  ? Args[2]['result']
-  : Args[2] extends (qb: Db) => Query
-  ? ReturnType<Args[2]>['result']
-  : Args[1] extends ColumnsShape
-  ? Args[1]
-  : Args[2] extends ColumnsShape
-  ? Args[2]
-  : Args[2] extends (t: ColumnTypes) => ColumnsShape
-  ? ReturnType<Args[2]>
-  : never;
-
-type WithResult<
-  T extends Query,
-  Args extends WithArgs,
-  Shape extends ColumnsShape,
-> = AddQueryWith<
-  T,
-  {
-    table: Args[0];
-    shape: Shape;
-    type: ColumnShapeOutput<Shape>;
-  }
->;
 
 type WindowResult<T extends Query, W extends WindowArg<T>> = SetQueryWindows<
   T,
@@ -263,7 +213,11 @@ const getClonedQueryData = <T extends Query>(
   return cloned;
 };
 
-export interface QueryMethods extends SelectMethods, FromMethod, JsonMethods {
+export interface QueryMethods
+  extends SelectMethods,
+    FromMethod,
+    WithMethod,
+    JsonMethods {
   then: Then<unknown>;
 }
 
@@ -453,51 +407,6 @@ export class QueryMethods {
       'as',
       tableAlias,
     ) as unknown as SetQueryTableAlias<T, TableAlias>;
-  }
-
-  with<
-    T extends Query,
-    Args extends WithArgs,
-    Shape extends ColumnsShape = WithShape<Args>,
-  >(this: T, ...args: Args): WithResult<T, Args, Shape> {
-    return this.clone()._with<T, Args, Shape>(...args);
-  }
-
-  _with<
-    T extends Query,
-    Args extends WithArgs,
-    Shape extends ColumnsShape = WithShape<Args>,
-  >(this: T, ...args: Args): WithResult<T, Args, Shape> {
-    let options =
-      (args.length === 3 && !isRaw(args[2])) || args.length === 4
-        ? (args[1] as WithArgsOptions | WithOptions)
-        : undefined;
-
-    const last = args[args.length - 1] as
-      | Query
-      | ((qb: Db) => Query)
-      | RawExpression;
-
-    const query = typeof last === 'function' ? last(this.queryBuilder) : last;
-
-    const shape =
-      args.length === 4 ? (args[2] as ColumnsShape) : (query as Query).shape;
-
-    if (options?.columns === true) {
-      options = {
-        ...options,
-        columns: Object.keys(shape),
-      };
-    }
-
-    pushQueryValue(this, 'with', [args[0], options || EMPTY_OBJECT, query]);
-
-    return setQueryObjectValue(
-      this,
-      'withShapes',
-      args[0],
-      shape,
-    ) as unknown as WithResult<T, Args, Shape>;
   }
 
   withSchema<T extends Query>(this: T, schema: string): T {
@@ -769,4 +678,4 @@ export class QueryMethods {
 
 QueryMethods.prototype.then = thenAll;
 
-applyMixins(QueryMethods, [SelectMethods, FromMethod, JsonMethods]);
+applyMixins(QueryMethods, [SelectMethods, FromMethod, WithMethod, JsonMethods]);
