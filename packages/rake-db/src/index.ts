330bdc1df45a4c3616cf6c29bbc7784de17db8cd
romeerez <romadzao@gmail.com>
2022-10-17 14:11:21 -0400

    Add exports to rake-db

--- packages/rake-db/src/index.ts
+++ packages/rake-db/src/index.ts
@@ -1,2 +1,5 @@
 export * from './commands/createOrDrop';
+export * from './commands/generate';
+export * from './commands/migrateOrRollback.test';
 export { change } from './migration/change';
+export * from './migration/migration';
