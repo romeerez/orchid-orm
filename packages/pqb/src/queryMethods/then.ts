67bbcdb5533d60dda3c238a2bfb9c45897a42bc3
romeerez <romadzao@gmail.com>
2022-10-22 09:55:17 -0400

    Remove this type from ThenResult to fix annoying TS errors

--- packages/pqb/src/queryMethods/then.ts
+++ packages/pqb/src/queryMethods/then.ts
@@ -6,8 +6,7 @@ import { CommonQueryData, Sql } from '../sql';
 import { AfterCallback, BeforeCallback } from './callbacks';
 import { getValueKey } from './get';
 
-export type ThenResult<Res> = <T extends Query>(
-  this: T,
+export type ThenResult<Res> = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   resolve?: (value: Res) => any,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
