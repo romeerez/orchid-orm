1f4821ab7f778624ee5b231c386384f75b8013f5
romeerez <romadzao@gmail.com>
2022-11-29 10:26:16 -0500

    Allow migrating in rake-db without test database url

--- packages/rake-db/db.ts
+++ packages/rake-db/db.ts
@@ -1,21 +1,25 @@
 import { config } from 'dotenv';
 import path from 'path';
 import { rakeDb } from './src/rakeDb';
+import { AdapterOptions } from 'pqb';
 
 config({ path: path.resolve(process.cwd(), '.env.local') });
 config();
+
+const options: AdapterOptions[] = [];
 
 const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
   throw new Error('DATABASE_URL is missing in .env');
 }
 
+options.push({ connectionString });
+
 const connectionStringTest = process.env.DATABASE_URL_TEST;
-if (!connectionStringTest) {
-  throw new Error('DATABASE_URL_TEST is missing in .env');
+if (connectionStringTest) {
+  options.push({ connectionString: connectionStringTest });
 }
 
-rakeDb(
-  [{ connectionString }, { connectionString: connectionStringTest }],
-  { migrationsPath: path.resolve(process.cwd(), 'migrations') },
-);
+rakeDb(options, {
+  migrationsPath: path.resolve(process.cwd(), 'migrations'),
+});
