31513216926107d9ab9803cfed94a9a7c5baa9fb
romeerez <romadzao@gmail.com>
2022-11-30 06:53:18 -0500

    Add script to output coverage total

---
+++ scripts/getTestCoverageTotal.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import readline from 'readline';
+
+const getFirstLine = async (path: string) => {
+  const readable = fs.createReadStream(path);
+  const reader = readline.createInterface({ input: readable });
+  const line = await new Promise((resolve) => {
+    reader.on('line', (line) => {
+      reader.close();
+      resolve(line);
+    });
+  });
+  readable.close();
+  return line;
+};
+
+const calculatePackageCoverage = async (name: string) => {
+  const path = `packages/${name}/coverage/coverage-summary.json`;
+  const data = JSON.parse(`${await getFirstLine(path)}}`);
+  return data.total.statements.pct;
+};
+
+const main = async () => {
+  const values = await Promise.all(
+    ['orm', 'pqb', 'rake-db', 'schema-to-zod', 'test-factory'].map(
+      calculatePackageCoverage,
+    ),
+  );
+  const value = values.reduce((acc, value) => acc + value, 0) / values.length;
+  process.stdout.write(`${Math.floor(value * 100) / 100}%`);
+};
+
+main();
