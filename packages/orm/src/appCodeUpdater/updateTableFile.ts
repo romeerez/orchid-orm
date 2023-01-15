a0e70e8a2f4ccdaf4f5a963ad04d813252f93ea8
romeerez <romadzao@gmail.com>
2023-01-15 02:23:00 -0500

    wip: minor changes for addCodeUpdater

---
+++ packages/orm/src/appCodeUpdater/updateTableFile.ts
@@ -0,0 +1,19 @@
+// import { RakeDbAst } from 'rake-db';
+//
+// export const updateTableFile = async (
+//   tablePath: (name: string) => string,
+//   ast: RakeDbAst,
+// ) => {
+//   if (ast.type === 'table') {
+//     if (ast.action === 'create') {
+//       createTable(tablePath, ast);
+//     }
+//   }
+// };
+//
+// const createTable = (
+//   tablePath: (name: string) => string,
+//   ast: RakeDbAst.Table,
+// ) => {
+//
+// };
