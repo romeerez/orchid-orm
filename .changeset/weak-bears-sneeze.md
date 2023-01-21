83cb186dd174eb37a79cb9574f7daf591b8b4269
romeerez <romadzao@gmail.com>
2023-01-20 22:31:15 -0500

    Fix export in pqb

---
+++ .changeset/weak-bears-sneeze.md
@@ -0,0 +1,5 @@
+---
+'pqb': patch
+---
+
+Fix export
--- packages/pqb/src/columnSchema/index.ts
+++ packages/pqb/src/columnSchema/index.ts
@@ -10,14 +10,4 @@ export * from './enum';
 export * from './json';
 export * from './array';
 export * from './virtual';
-export { Code, codeToString, columnsShapeToCode } from './code';
-export { columnCode } from './code';
-export { foreignKeyArgumentToCode } from './code';
-export { columnDefaultArgumentToCode } from './code';
-export {
-  columnChainToCode,
-  addCode,
-  primaryKeyToCode,
-  indexToCode,
-  foreignKeyToCode,
-} from './code';
+export * from './code';
