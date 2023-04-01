61c34609b0eddc70248668f64eba34d46148df01
romeerez <romadzao@gmail.com>
2023-04-01 18:11:56 -0400

    Fix test factory test

--- packages/test-factory/src/test-utils.ts
+++ packages/test-factory/src/test-utils.ts
@@ -22,7 +22,7 @@ export const BaseTable = createBaseTable({
   columnTypes: (t) => ({
     ...t,
     text: (min = 0, max = Infinity) => t.text(min, max),
-    timestampWithoutTimeZone: () => t.timestampWithoutTimeZone().asNumber(),
+    timestamp: () => t.timestamp().asNumber(),
   }),
 });
 
