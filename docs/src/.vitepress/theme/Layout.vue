51506dbfa0e0c007aa091177e63016229aedb9ef
bingtsingw <10382462+bingtsingw@users.noreply.github.com>
2023-04-26 17:19:18 +0800

    format vitepress theme

--- docs/src/.vitepress/theme/Layout.vue
+++ docs/src/.vitepress/theme/Layout.vue
@@ -1,53 +1,56 @@
 <script setup>
-import DefaultTheme from 'vitepress/theme'
+import DefaultTheme from 'vitepress/theme';
 import { useRouter } from 'vitepress';
-import { watch } from "vue";
+import { watch } from 'vue';
 
 // localStorage and window are not available during the build
-const isBuild = typeof window === 'undefined'
+const isBuild = typeof window === 'undefined';
 
 if (!isBuild && !localStorage.getItem('vitepress-theme-appearance')) {
-  document.documentElement.classList.add('dark')
-  localStorage.setItem('vitepress-theme-appearance', 'dark')
+  document.documentElement.classList.add('dark');
+  localStorage.setItem('vitepress-theme-appearance', 'dark');
 }
 
 const router = useRouter();
 
 // Only run this on the client. Not during build.
 if (!isBuild) {
-  window.dataLayer = window.dataLayer || []
-  function gtag(){ dataLayer.push(arguments) }
-  gtag('js', new Date())
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
 
-  const analyticsId = 'G-PV4PL9TK79'
-  gtag('config', analyticsId)
+  const analyticsId = 'G-PV4PL9TK79';
+  gtag('config', analyticsId);
 
-  watch(() => router.route.data.relativePath, () => {
-    setTimeout(() => {
-      gtag('event', 'page_view', {
-        page_title: document.title,
-        page_location: window.location.href,
-        page_path: window.location.pathname,
-        send_to: analyticsId,
+  watch(
+    () => router.route.data.relativePath,
+    () => {
+      setTimeout(() => {
+        gtag('event', 'page_view', {
+          page_title: document.title,
+          page_location: window.location.href,
+          page_path: window.location.pathname,
+          send_to: analyticsId,
+        });
       });
-    })
-  }, { immediate: true });
+    },
+    { immediate: true },
+  );
 }
 
-const { Layout } = DefaultTheme
+const { Layout } = DefaultTheme;
 </script>
 
 <template>
   <Layout>
     <template v-slot:nav-bar-content-before>
-      <a
-        href='https://stand-with-ukraine.pp.ua/'
-        class='stand-with-ukraine'
-      >
-        <div class='flag'></div>
-        <div class='text'>
-          <span class='stand-with'>Stand With </span>
-          <span class='Ukraine'>Ukraine</span>
+      <a href="https://stand-with-ukraine.pp.ua/" class="stand-with-ukraine">
+        <div class="flag"></div>
+        <div class="text">
+          <span class="stand-with">Stand With </span>
+          <span class="Ukraine">Ukraine</span>
         </div>
       </a>
     </template>
