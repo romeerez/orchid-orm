eb92c705b54bfba50dd3799556705671beffee2b
Roman Kushyn <romadzao@gmail.com>
2026-02-15 20:54:37 +0100

    Expose `OrderArg` type (#643)

---
+++ .changeset/quick-waves-arrive.md
@@ -0,0 +1,5 @@
+---
+'pqb': patch
+---
+
+Expose `OrderArg` type (#643)
--- packages/pqb/src/query/index.ts
+++ packages/pqb/src/query/index.ts
@@ -16,6 +16,8 @@ export * from './basic-features/mutate/or-create';
 export * from './basic-features/mutate/update';
 export * from './basic-features/mutate/upsert';
 export * from './basic-features/order/order.sql';
+// requested in https://github.com/romeerez/orchid-orm/issues/643
+export { OrderArg } from './basic-features/order/order';
 export * from './basic-features/schema/schema';
 export * from './basic-features/select/delayed-relational-select';
 export * from './basic-features/select/hook-select';
